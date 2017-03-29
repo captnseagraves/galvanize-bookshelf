3f0fe19b299d653cf514472df469fc991a2e6abf
Kevin Seagraves <Kevin Seagraves>
2017-03-29 16:15:01 -0600

    completed through part3

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,105 @@
 'use strict';
 
 const express = require('express');
+const knex = require('../knex');
+const humps = require('humps');
+const boom = require('boom')
 
-// eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get("/books", (req, res, next) => {
+  knex("books")
+  .orderBy("title", "asc")
+    .then((books) => {
+      res.send(humps.camelizeKeys(books));
+    });
+});
+router.get("/books/:id", (req, res, next) => {
+  let id = req.params.id
+  if (id !== 'id') {
+     return res.status(404)
+               .set('Content-Type', 'text/plain')
+               .send('Not Found')
+   }
+  knex("books")
+  .where("id", req.params.id)
+  .then((book) => {
+    res.send(humps.camelizeKeys(book[0]));
+  });
+});
+
+router.post('/books', (req, res, next) => {
+  let title = req.body.title;
+  if (!title) {
+     return res.status(400)
+               .set('Content-Type', 'text/plain')
+               .send('Title must not be blank')
+   };
+   let author = req.body.author;
+   if (!author) {
+      return res.status(400)
+                .set('Content-Type', 'text/plain')
+                .send('Author must not be blank')
+    };
+    let genre = req.body.genre;
+    if (!genre) {
+       return res.status(400)
+                 .set('Content-Type', 'text/plain')
+                 .send('Genre must not be blank')
+     };
+     let description = req.body.description;
+     if (!description) {
+        return res.status(400)
+                  .set('Content-Type', 'text/plain')
+                  .send('Description must not be blank')
+      };
+      let coverUrl = req.body.coverUrl;
+      if (!coverUrl) {
+         return res.status(400)
+                   .set('Content-Type', 'text/plain')
+                   .send('Cover URL must not be blank')
+       };
+  knex('books')
+  .returning('*')
+  .insert(humps.decamelizeKeys(req.body))
+  .then((book) =>{
+    res.send(humps.camelizeKeys(book[0]))
+  })
+})
+
+router.patch('/books/:id', (req, res, next) => {
+  let id = req.params.id
+  if (id !== 'id') {
+    return next(boom.notFound('Not Found'));
+   }
+  knex('books')
+  .returning('*')
+  .update(humps.decamelizeKeys(req.body))
+  .then((book) =>{
+    res.send(humps.camelizeKeys(book[0]))
+  })
+})
+
+router.delete('/books/:id', (req, res, next) => {
+  let id = req.params.id
+  if (id !== 'id') {
+     return res.status(404)
+               .set('Content-Type', 'text/plain')
+               .send('Not Found')
+   }
+  knex('books')
+  .returning(['title', 'author', 'genre', 'description', 'cover_url'])
+  .del(humps.decamelizeKeys(req.body))
+  .then((book) =>{
+    res.send(humps.camelizeKeys(book[0]))
+  })
+})
+
+// router.get('/books', (res, req, next) => {
+//   knex('books')
+//   .then(book)
+//   res.send(humps.camelizeKeys(book[0]))
+// })
+//
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,32 @@
 'use strict';
 
 const express = require('express');
+const humps = require('humps');
+const bcrypt = require('bcrypt');
+const boom = require('boom')
+const knex = require('../knex')
+
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.post('/users', (req, res, next) => {
+  console.log(req.body);
+  // let fName = req.body.first_name;
+  // if (!fName) {
+  //   return next(boom.badRequest('First name must not be blank'))
+  // }
+  knex('users')
+  .returning(['id', 'first_name', 'last_name', 'email'])
+  .insert({
+    first_name: req.body.firstName,
+    last_name: req.body.lastName,
+    email: req.body.email,
+    hashed_password: bcrypt.hashSync(req.body.password, 8)
+  })
+  .then((user) =>{
+    res.send(humps.camelizeKeys(user[0]))
+  })
+})
 
 module.exports = router;
